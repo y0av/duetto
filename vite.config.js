06fe5e353c2b614ddb7c7ce869162111bfc30b80
y0av <mail.yoav@gmail.com>
2025-07-31 18:33:01 +0300

    vite config

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,14 @@
 import { defineConfig } from 'vite'
 
 export default defineConfig({
+  server: {
+    // Allow all hosts - Firebase App Hosting generates new host IDs for each deployment
+    host: true,
+    allowedHosts: 'all'
+  },
   preview: {
     // Allow all hosts - Firebase App Hosting generates new host IDs for each deployment
+    host: true,
     allowedHosts: 'all'
   }
 })
